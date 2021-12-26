86a2f81b6684bb314409de86dda41c443d8ccafe
naokisomeya <sometimes.b0105@gmail.com>
2021-12-26 19:29:03 +0900

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  const componentName = () => 'Header'
+  return (
+    <div>テスト</div>
+    <div className="test-wrapper">
+      テスト
+      このコンポーネントの名前は{componentName()}です。
+    </div>
+
+  );
+};
