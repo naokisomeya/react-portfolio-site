974873aff22f87bc450fd363161eb256ce09fab5
naokisomeya <sometimes.b0105@gmail.com>
2021-12-26 20:32:44 +0900

    contact終了

---
+++ src/components/Skills.jsx
@@ -0,0 +1,15 @@
+export const Skills = () => {
+  return (
+    <div id="skill">
+      <div className="container">
+        <div className="heading">
+          <h2>Skills</h2>
+        </div>
+        <div className="skills-container">
+
+        </div>
+      </div>
+
+    </div>
+  );
+};
